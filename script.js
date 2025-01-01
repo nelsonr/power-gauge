8338070825caad64252b3f4367112ca20d43ab18
Nelson Rodrigues <nelsonrodrigues@hi-interactive.com>
2025-01-01 19:12:09 +0000

    Add hit frames

--- script.js
+++ script.js
@@ -8,25 +8,38 @@ let hit = false;
 
 let prevTime = 0;
 let interval = 1000 / 60;
+let hitFrames = 8;
 
 function onClick () {
-    power = power + 0.15;
-    power = power >= 1.0 ? 1.0 : power;
+    if (!hit) {
+        power = power + 0.15;
+        power = power >= 1.0 ? 1.0 : power;
+        power = power > target ? target + 0.01 : power;
+    }
 }
 
 function update (dt) {
     if (power > target) {
         hit = true;
-        target = -1.0;
     }
 
-    power = power - ((1.8 - power) * 0.01);
-    power = power <= 0.0 ? 0.0 : power;
+    if (hit && hitFrames > 0) {
+        hitFrames--;
 
-    if (hit && power <= 0.40) {
-        hit = false;
-        target = Math.random() + 0.40;
-        target = target >= 1.0 ? 0.95 : target;
+        if (hitFrames === 0) {
+            target = -1.0;
+        }
+    } else {
+        power = power - ((1.8 - power) * 0.01);
+        power = power <= 0.0 ? 0.0 : power;
+
+        if (hit && power <= 0.40) {
+            hitFrames = 10;
+            hit = false;
+            target = Math.random() + 0.40;
+            target = target >= 1.0 ? 0.9 : target;
+        }
+
     }
 }
 
